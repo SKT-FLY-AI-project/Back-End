1e8b22a50d9f1a2bacef80f97cdad440297517aa
kwakrhkr59 <kwakrhkr59@outlook.com>
2025-02-18 00:44:38 +0900

    detect API 인코딩 방식 변경

--- app/api/routes/photo.py
+++ app/api/routes/photo.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, UploadFile, File, Depends
-from fastapi.responses import StreamingResponse
+from fastapi.responses import JSONResponse
+import io
 from PIL import Image
 import numpy as np
 from app.config import UPLOAD_DIR
@@ -7,6 +8,7 @@ from app.utils.s3_utils import upload_to_s3
 from app.utils.model_utils import model_manager
 from app.utils.image_utils import detect_painting_region, preprocess_image
 
+import base64
 import io
 import os
 
@@ -27,12 +29,14 @@ async def upload(userid: str, file: UploadFile = File(...)):
     if s3_url:
         return {"message": "File uploaded successfully", "s3_url": s3_url}
     return {"error": "Failed to upload to S3"}
+\
 
-@router.post("/detect/")
+@router.post("/detect")
 async def detect(file: UploadFile = File(...)):
     print("Detect:", file.content_type)
+    
     if not file.content_type.startswith('image'):
-        return {"error": "Invalid file type. Please upload an image."}
+        return JSONResponse(content={"error": "Invalid file type. Please upload an image."}, status_code=400)
     
     contents = await file.read()
     image = Image.open(io.BytesIO(contents)).convert("RGB")
@@ -41,8 +45,12 @@ async def detect(file: UploadFile = File(...)):
     img_byte_arr = io.BytesIO()
     painting.save(img_byte_arr, format='PNG')
     img_byte_arr.seek(0)
+
+    # ✅ Base64로 인코딩하여 반환
+    encoded_image = base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
     
-    return StreamingResponse(img_byte_arr, media_type="image/png")
+    return JSONResponse(content={"image": encoded_image})
+
 
 @router.post("/classify/{userid}")
 async def classify(userid: str, file: UploadFile = File(...)):
@@ -51,16 +59,16 @@ async def classify(userid: str, file: UploadFile = File(...)):
     
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
-    # painting = detect_painting_region(image)
+    painting = detect_painting_region(image)
     
-    processed_image = preprocess_image(image)
+    processed_image = preprocess_image(painting)
     model = model_manager.load_model()
     prediction = model.predict(processed_image)
     predicted_class = int(np.argmax(prediction, axis=1)[0])
     
     output_filename = f"{userid}/detected{predicted_class}_{file.filename}"
     output_path = os.path.join(UPLOAD_DIR, output_filename)
-    image.save(output_path)
+    painting.save(output_path)
     
     s3_url = upload_to_s3(userid, output_path, output_filename)
     if s3_url:

11e469a8eef78fe4231bb6a09d5898933567c48e
kwakrhkr59 <kwakrhkr59@outlook.com>
2025-02-17 20:25:15 +0900

    detect API 함수 추가

--- app/api/routes/photo.py
+++ app/api/routes/photo.py
@@ -28,6 +28,22 @@ async def upload(userid: str, file: UploadFile = File(...)):
         return {"message": "File uploaded successfully", "s3_url": s3_url}
     return {"error": "Failed to upload to S3"}
 
+@router.post("/detect/")
+async def detect(file: UploadFile = File(...)):
+    print("Detect:", file.content_type)
+    if not file.content_type.startswith('image'):
+        return {"error": "Invalid file type. Please upload an image."}
+    
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    painting = detect_painting_region(image)
+    
+    img_byte_arr = io.BytesIO()
+    painting.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+    
+    return StreamingResponse(img_byte_arr, media_type="image/png")
+
 @router.post("/classify/{userid}")
 async def classify(userid: str, file: UploadFile = File(...)):
     if not file.content_type.startswith("image"):
@@ -35,16 +51,16 @@ async def classify(userid: str, file: UploadFile = File(...)):
     
     contents = await file.read()
     image = Image.open(io.BytesIO(contents))
-    painting = detect_painting_region(image)
+    # painting = detect_painting_region(image)
     
-    processed_image = preprocess_image(painting)
+    processed_image = preprocess_image(image)
     model = model_manager.load_model()
     prediction = model.predict(processed_image)
     predicted_class = int(np.argmax(prediction, axis=1)[0])
     
     output_filename = f"{userid}/detected{predicted_class}_{file.filename}"
     output_path = os.path.join(UPLOAD_DIR, output_filename)
-    painting.save(output_path)
+    image.save(output_path)
     
     s3_url = upload_to_s3(userid, output_path, output_filename)
     if s3_url:
